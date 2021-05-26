3f0c10140db34de6081f23f3d92307307d3cc530
sagarvanave <ssv1437@gmail.com>
2021-05-26 12:10:20 +0530

    CSS is added in page

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 
 
 
-app.get('',(req, res) =>{
+app.get('/',(req, res) =>{
     res.render('index',{
         title:'Weather App',
         name:'Sagar Vanave'
